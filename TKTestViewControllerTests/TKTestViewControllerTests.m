151fe702cbe091bc1c114fe2ee686a77e9c161fb
Trieu Khang <hoangtrieukhang@gmnail.com>
2014-04-09 00:41:08 +0700

    Put simple initialisation view controller test

--- TKTestViewControllerTests/TKTestViewControllerTests.m
+++ TKTestViewControllerTests/TKTestViewControllerTests.m
@@ -7,9 +7,28 @@
 //
 
 #import "Specta.h"
+#define EXP_SHORTHAND
+#import "TKViewController.h"
+#import "Expecta.h"
+#import "OCMock.h"
 
-SpecBegin(ViewControllerSpec)
+
+SpecBegin(TKViewController)
 
 describe(@"View Controller", ^{
 
-})
+    __block TKViewController *vc;
+    beforeEach(^{
+        UIStoryboard *storyBoard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+        UINavigationController *navVC = [storyBoard instantiateInitialViewController];
+        vc = (TKViewController *)[navVC visibleViewController];
+    });
+
+    if(@"Shoud be init from story board", ^{
+        expect(vc).toNot.beNil();
+        expect(vc).to.beInstanceOf([UIView class]);
+    });
+
+});
+
+SpecEnd
