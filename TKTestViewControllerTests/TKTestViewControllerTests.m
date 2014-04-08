163193f83baf8419d8fb71e8452a9e36c8b5d528
Trieu Khang <hoangtrieukhang@gmnail.com>
2014-04-09 00:18:50 +0700

    Now can run test

--- TKTestViewControllerTests/TKTestViewControllerTests.m
+++ TKTestViewControllerTests/TKTestViewControllerTests.m
@@ -6,29 +6,10 @@
 //  Copyright (c) 2014 Triệu Khang. All rights reserved.
 //
 
-#import <XCTest/XCTest.h>
+#import "Specta.h"
 
-@interface TKTestViewControllerTests : XCTestCase
+SpecBegin(ViewControllerSpec)
 
-@end
+describe(@"View Controller", ^{
 
-@implementation TKTestViewControllerTests
-
-- (void)setUp
-{
-    [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-}
-
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
-
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}
-
-@end
+})
